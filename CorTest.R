c4d7098a41a2e98ae570024b20eee27b645dd200
nzminki0621 <65962055+nzminki0621@users.noreply.github.com>
2020-08-27 22:21:07 +0900

    Add files via upload

---
+++ CorTest.R
@@ -0,0 +1,7 @@
+app_store = read.csv("/Users/imingi/Documents/code/growth_hackers/app_store_modified.csv")
+
+
+cor.test(app_store$Reviews,app_store$Rating, method = "pearson")
+cor.test(app_store$Reviews_logscale,app_store$Rating, method = "pearson")
+cor.test(app_store$Installs,app_store$Rating, method = "pearson")
+cor.test(app_store$Installs_logscale,app_store$Rating, method = "pearson")
